16c575d45bc1b5172aa10ebcc243bc93587715d5
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-07-02 06:23:05 -0700

    Created initial projects and classes

---
+++ PasswordManagerApp/PasswordManagerApp/PasswordManagerUser.cs
@@ -0,0 +1,22 @@
+﻿using PasswordManagerInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordManagerApp
+{
+    public class PasswordManagerUser
+    {
+        public string Name { get; set; }
+        public ICollection<IPasswordProperties> Passwords { get; set; }
+        public byte[] PasswordHash { get; set; }
+        private HashAlgorithm hashAlgorithm;
+        public bool CheckPassword(string password)
+        {
+            byte[] hashResult = hashAlgorithm.ComputeHash(Encoding.ASCII.GetBytes(password));
+            return hashResult.Zip(PasswordHash, (r, p) => r == p).All(x => x);
+        }
+    }
+}
---
+++ PasswordManagerApp/PasswordManagerInterfaces/IPasswordManagerUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordManagerInterfaces
+{
+    public interface IPasswordManagerUser
+    {
+        string Name { get; }
+        ICollection<IPasswordProperties> Passwords { get; }
+        bool CheckPassword(string password);
+    }
+}
