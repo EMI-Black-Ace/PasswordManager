79cb1b3df090431207348f062cea4344023db1cd
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-07-10 06:16:08 -0700

    Working on PasswordProperties

--- PasswordManagerApp/PasswordManagerApp/PasswordProperties.cs
+++ PasswordManagerApp/PasswordManagerApp/PasswordProperties.cs
@@ -17,16 +17,26 @@ namespace PasswordManagerApp
         private readonly IPasswordManagerUser passwordManagerUser;
         private Guid passwordSeed = Guid.NewGuid();
 
+        /// <summary>
+        /// Changes the stored randomizer seed involved in creating the password.  Not reversible.
+        /// </summary>
         public void ChangeThisPassword()
         {
             passwordSeed = Guid.NewGuid();
         }
 
+        /// <summary>
+        /// Generates a password mingling the user's master password, stored hash, internal randomizer and object properties
+        /// </summary>
+        /// <param name="masterPassword">The user's master password</param>
+        /// <returns>a generated password</returns>
         public string GeneratePassword(string masterPassword)
         {
             throw new NotImplementedException();
         }
 
-        
+        public override string ToString() => $"{passwordManagerUser.Name}: {Name}";
+
+        public PasswordProperties(IPasswordManagerUser passwordManagerUser) => this.passwordManagerUser = passwordManagerUser ?? throw new ArgumentNullException(nameof(passwordManagerUser));
     }
 }
