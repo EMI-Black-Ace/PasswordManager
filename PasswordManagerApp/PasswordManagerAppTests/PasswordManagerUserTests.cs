6ff68ea895cae194b402efe619d4286277bfec7d
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-07-11 06:02:15 -0700

    started on PasswordManagerUserTests

---
+++ PasswordManagerApp/PasswordManagerAppTests/PasswordManagerUserTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManagerAppTests
+{
+    public class PasswordManagerUserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void HashConsistencyTest()
+        {
+
+        }
+    }
+}
