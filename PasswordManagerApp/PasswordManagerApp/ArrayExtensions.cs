f947520c98f32a8959977f3d7f5189a78187840c
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-09-09 12:45:55 -0700

    Created Array.ReduceOrExpand function and proved it works as expected

--- PasswordManagerApp/PasswordManagerApp/ArrayExtensions.cs
+++ PasswordManagerApp/PasswordManagerApp/ArrayExtensions.cs
@@ -23,5 +23,53 @@ namespace PasswordManagerApp
 
             return true;
         }
+
+        /// <summary>
+        /// Expands or contracts an array using all the values of the array to generate new values
+        /// </summary>
+        /// <typeparam name="T">Any type</typeparam>
+        /// <param name="items"></param>
+        /// <param name="newLength">The length of the new array</param>
+        /// <param name="combinerFunction">A method to combine elements, i.e. (x,y)=>x+y;</param>
+        /// <returns></returns>
+        public static T[] ReduceOrExpand<T>(this T[] items, int newLength, Func<T,T,T> combinerFunction)
+        {
+            if(items.Length == newLength)
+            {
+                return (T[])items.Clone();
+            }
+
+            T[] array = new T[newLength];
+
+            //copy the values over into the new array
+            for (int i = 0; i < Math.Min(items.Length, newLength); ++i)
+            {
+                array[i] = items[i];
+            }
+
+            if (newLength > items.Length)
+            {
+                //accumulate the old array and scroll to get the new values
+                for(int i = items.Length; i < newLength; ++i)
+                {
+                    T totalValue = items[i % items.Length];
+                    for(int j = i - items.Length; j < i; ++j)
+                    {
+                        totalValue = combinerFunction(array[j], totalValue);
+                    }
+                    array[i] = totalValue;
+                }
+            }
+            else
+            {
+                //accumulate the excess items into the earlier items
+                for(int i = 0; i < items.Length - newLength; ++i)
+                {
+                    array[i % newLength] = combinerFunction(array[i % newLength], items[i + newLength]);
+                }
+            }
+
+            return array;
+        }
     }
 }
--- PasswordManagerApp/PasswordManagerAppTests/ArrayExtensionsTests.cs
+++ PasswordManagerApp/PasswordManagerAppTests/ArrayExtensionsTests.cs
@@ -38,5 +38,31 @@ namespace PasswordManagerAppTests
             Assert.IsFalse(array1.AllItemsAreEqual(array2));
             Assert.IsFalse(array2.AllItemsAreEqual(array1));
         }
+
+        [Test]
+        public void ReduceOrExpand_EqualLength_ReturnsCopyOfOriginal()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] newArray = array.ReduceOrExpand(5, (x, y) => x + y);
+            Assert.IsTrue(array.AllItemsAreEqual(newArray));
+        }
+
+        [Test]
+        public void ReduceOrExpand_Larger_LengthMatchesSpecAndItemsFilled()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] newArray = array.ReduceOrExpand(20, (x, y) => x + y);
+            Assert.AreEqual(20, newArray.Length);
+            Assert.IsFalse(array.Any((x) => x == 0));
+        }
+
+        [Test]
+        public void ReduceOrExpand_Smaller_LengthMatchesSpecAndItemsUnique()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            int[] newArray = array.ReduceOrExpand(5, (x, y) => x + y);
+            Assert.AreEqual(5, newArray.Length);
+            CollectionAssert.IsNotSubsetOf(newArray, array);
+        }
     }
 }
