c2425b542e55b755436dce0fcb80bceeaad85aab
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-11-26 14:04:34 -0700

    Initial add of PasswordProperties test

---
+++ PasswordManagerApp/PasswordManagerAppTests/PasswordPropertiesTests.cs
@@ -0,0 +1,18 @@
+﻿using PasswordManagerApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManagerAppTests
+{
+    public class PasswordPropertiesTests
+    {
+        private PasswordManagerUser user;
+        private PasswordProperties password;
+        
+        public PasswordPropertiesTests()
+        {
+
+        }
+    }
+}
