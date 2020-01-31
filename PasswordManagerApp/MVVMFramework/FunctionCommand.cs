6cbbf9894743a260658747b12d8f419d59255c39
Jacob Van Wagoner <veedubforlife@gmail.com>
2020-01-30 22:19:38 -0700

    Working more on main viewmodel, added FunctionCommand (basic ICommand implementation for executing a lambda)

---
+++ PasswordManagerApp/MVVMFramework/FunctionCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVMFramework
+{
+    public class FunctionCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> executeAction = null;
+        private Predicate<object> executeCheck = null;
+
+        public bool CanExecute(object parameter)
+        {
+            return executeCheck?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                executeAction?.Invoke(parameter);
+            }
+        }
+
+        /// <summary>
+        /// Instance of FunctionCommand
+        /// </summary>
+        /// <param name="function">The function to execute when Execute() is called</param>
+        /// <param name="condition">The function to check whether Execute() is valid</param>
+        public FunctionCommand(Action<object> function, Predicate<object> condition = null)
+        {
+            executeAction = function;
+            executeCheck = condition;
+        }
+    }
+}
--- PasswordManagerApp/PasswordManagerUI/PasswordManagerUI/PasswordGenerationViewModel.cs
+++ PasswordManagerApp/PasswordManagerUI/PasswordManagerUI/PasswordGenerationViewModel.cs
@@ -1,13 +1,65 @@
-﻿using PasswordManagerApp;
+﻿using MVVMFramework;
+using PasswordManagerApp;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace PasswordManagerUI
 {
-    public class PasswordGenerationViewModel
+    public class PasswordGenerationViewModel : ViewModel
     {
+        private PasswordManagerUser selectedUser;
+        private PasswordProperties selectedPassword;
+        private string masterPassword;
+        private string generatedPassword;
+        private ICommand createUserCommand;  //TODO:  Replace with abstract
+        private ICommand createPasswordCommand;  //TODO:  Replace with abstract
+        private FunctionCommand generatePasswordCommand;
+
         public ObservableCollection<PasswordManagerUser> Users { get; internal set; }
+        public PasswordManagerUser SelectedUser { get => selectedUser; set => SetField(ref selectedUser, value); }
+        public PasswordProperties SelectedPassword { get => selectedPassword; set => SetField(ref selectedPassword, value); }
+        public string MasterPassword { get => masterPassword; set => SetField(ref masterPassword, value); }
+        public string GeneratedPassword { get => generatedPassword; set => SetField(ref generatedPassword, value); }
+
+        public ICommand CreateUserCommand
+        {
+            get
+            {
+                if(createUserCommand == null)
+                {
+                    createUserCommand = null; //TODO:  User Creation Service
+                }
+                return createUserCommand;
+            }
+        }
+
+        public ICommand CreateNewPasswordCommand
+        {
+            get
+            {
+                if(createPasswordCommand == null)
+                {
+                    createPasswordCommand = null; //TODO:  Password Creation Service
+                }
+                return createPasswordCommand;
+            }
+        }
+
+        public ICommand GeneratePasswordCommand
+        {
+            get
+            {
+                if(generatePasswordCommand == null)
+                {
+                    generatePasswordCommand = new FunctionCommand(
+                        p => GeneratedPassword = selectedPassword.GeneratePassword(masterPassword),
+                        p => selectedPassword != null);
+                }
+                return generatePasswordCommand;
+            }
+        }
     }
 }
