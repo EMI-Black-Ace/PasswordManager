3efafa76b3ea677b1170fb4f0ccbe28acc94d08c
Jacob Van Wagoner <veedubforlife@gmail.com>
2020-02-04 15:56:20 -0700

    Proved selecting a user raises appropriate notify events and updates the password selected

--- PasswordManagerApp/PasswordManagerUITests/PasswordGenerationViewModelTests.cs
+++ PasswordManagerApp/PasswordManagerUITests/PasswordGenerationViewModelTests.cs
@@ -1,7 +1,11 @@
 using Ninject;
 using Ninject.MockingKernel.Moq;
 using NUnit.Framework;
+using PasswordManagerApp;
 using PasswordManagerUI;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TestCommonLibrary;
 
 namespace PasswordManagerUITests
 {
@@ -9,17 +13,31 @@ namespace PasswordManagerUITests
     {
         private readonly MoqMockingKernel _automock = new MoqMockingKernel();
         private PasswordGenerationViewModel _vm;
+        private readonly System.Collections.Generic.List<string> _propertiesChanged = new System.Collections.Generic.List<string>();
 
         [SetUp]
         public void Setup()
         {
             _automock.Reset();
+            _propertiesChanged.RemoveAll(x => true);
         }
 
         [Test]
-        public void Test1()
+        public void SelectedUserChanges_NotifyEventRaised_SelectedPasswordUpdates()
         {
-            Assert.Pass();
+            _vm = GetVmInstance();
+            _vm.SelectedUser = TestPasswordManagerUsers.Instance.Users.First();
+
+            CollectionAssert.Contains(_vm.SelectedUser.Passwords, _vm.SelectedPassword);
+            CollectionAssert.Contains(_propertiesChanged, "SelectedUser");
+            CollectionAssert.Contains(_propertiesChanged, "SelectedPassword");
+        }
+
+        private PasswordGenerationViewModel GetVmInstance()
+        {
+            var vm = _automock.Get<PasswordGenerationViewModel>();
+            vm.PropertyChanged += (o, e) => _propertiesChanged.Add(e.PropertyName);
+            return vm;
         }
     }
 }
