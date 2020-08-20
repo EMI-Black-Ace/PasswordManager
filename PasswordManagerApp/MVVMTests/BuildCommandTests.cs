84bd7792d5a5263b78edaae45ddf9747d9d47554
Jacob Van Wagoner <veedubforlife@gmail.com>
2020-08-20 14:15:54 -0700

    orking on build commands, realized password length wasn't in the main page

---
+++ PasswordManagerApp/MVVMTests/BuildCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using MVVMFramework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMTests
+{
+    public class BuildCommandTests
+    {
+        public class IntBuilder : BuildCommand<int>
+        {
+            protected override int Build(object parameter)
+            {
+                return 0;
+            }
+        }
+
+        [Test]
+        public void BuildCommandExecute_RaisesEvent()
+        {
+            bool finished = false;
+            var b = new IntBuilder();
+            b.BuildFinished += i => finished = true;
+
+            Assert.IsTrue(finished);
+        }
+    }
+}
---
+++ PasswordManagerApp/PasswordManagerUI/PasswordManagerUI/PasswordCreate/PasswordCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVVMFramework;
+using PasswordManagerApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManagerUI.PasswordCreate
+{
+    public class PasswordCreateViewModel : ViewModel
+    {
+        public PasswordManagerUser User { get; private set; }
+        public string PasswordName { get; set; }
+        public bool 
+    }
+}
