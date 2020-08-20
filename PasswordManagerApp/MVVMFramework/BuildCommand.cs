48125e1ba58d61cfe1269057211ee7cc2be7806b
Jacob Van Wagoner <veedubforlife@gmail.com>
2020-08-20 09:17:54 -0700

    build password page now renders, created abstract build command

---
+++ PasswordManagerApp/MVVMFramework/BuildCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVMFramework
+{
+    public abstract class BuildCommand<T> : ICommand
+    {
+        protected abstract T Build(object parameter);
+
+        public event EventHandler CanExecuteChanged;
+        public event Action<T> BuildFinished;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            T builtObj = Build(parameter);
+            BuildFinished?.Invoke(builtObj);
+        }
+    }
+}
